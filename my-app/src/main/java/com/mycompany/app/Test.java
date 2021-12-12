44a3a8bee78e3ac50a90478aa1dccc2a681d5fdf
lab <lab@gmail.com>
2021-12-13 01:39:45 +0530

    Selenium stage added

---
+++ my-app/src/main/java/com/mycompany/app/Test.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+public class Test {
+
+	String message = "Hello World!";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ my-app/src/test/java/com/mycompany/app/SeleniumTest.java
@@ -0,0 +1,51 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import junit.framework.Assert;  
+  
+public class SeleniumTest {  
+  
+    public static void main(String[] args) {  
+        
+    // declaration and instantiation of objects/variables  
+    System.setProperty("webdriver.chrome.driver", "/home/lab/Lab/resources/chromedriver");  
+    WebDriver driver=new ChromeDriver();  
+      
+// Launch website  
+    driver.navigate().to("http://www.google.com/");  
+    System.out.println("navigated to google......");
+          
+    // Click on the search text box and send value  
+    driver.findElement(By.name("q")).sendKeys("https://www.spicejet.com/"); 
+    System.out.println("found lst-ib......");
+          
+    
+    WebDriverWait mywait=new WebDriverWait(driver,40);
+    WebElement ele=mywait.until(ExpectedConditions.elementToBeClickable(By.name("btnK")));
+    ele.click();
+    // Click on the search button  
+    Dimension txt = driver.findElement(By.name("q")).getSize(); 
+    System.out.println("txt...... " + txt.getHeight());
+    
+    // height is 39, assert height
+    
+    Assert.assertTrue(txt.getHeight() == 39);
+    
+    Test test = new Test();
+   
+    //System.out.println(test.getMessage());
+   Assert.assertTrue(test.getMessage().equals("Hello World!"));
+   
+   test.setMessage("bye bye");
+   Assert.assertFalse(test.getMessage().equals("Hello World!"));
+     
+    }  
+  
+}
