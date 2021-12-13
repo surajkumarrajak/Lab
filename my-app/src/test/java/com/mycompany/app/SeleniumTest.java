7caeddb55fb1af41e5e757d636027ad0012a48d2
lab <lab@gmail.com>
2021-12-13 16:03:16 +0530

    Selenium changes

--- my-app/src/test/java/com/mycompany/app/SeleniumTest.java
+++ my-app/src/test/java/com/mycompany/app/SeleniumTest.java
@@ -7,10 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
 
 import junit.framework.Assert;  
   
-public class SeleniumTest {  
+public class SeleniumTest  extends TestCase{  
+
   
     public static void main(String[] args) {  
         
