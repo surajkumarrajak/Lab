437730b659de0fe4127256f4db47218093baecb8
lab <lab@gmail.com>
2021-12-15 22:18:23 +0530

    Test final

--- my-app/src/test/java/com/mycompany/app/SeleniumTest.java
+++ my-app/src/test/java/com/mycompany/app/SeleniumTest.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the above line and uncomment below line to use Chrome
 //import org.openqa.selenium.chrome.ChromeDriver;
-public class PG1 {
+public class SeleniumTest {
 
 
     public static void main(String[] args) {
